0a87f7946c5f73ca15203b471a83cb368f738204
EdgarSargsyan <don.edgar@mail.ru>
2018-01-11 08:11:50 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$("#innerMenu1").on({
+	mouseleave: function () {
+	$("#hideUl1").slideUp("normal");
+	},
+	mouseenter: function(){
+	$("#hideUl1").slideDown("normal")
+	}
+});
+$("#innerMenu2").on({
+	mouseleave: function () {
+	$("#hideUl2").slideUp("normal");
+	},
+	mouseenter: function(){
+	$("#hideUl2").slideDown("normal")
+	}
+});
